5c3166f3f3ae897da8550420329c2c9c9f5981bb
Gildo Fiorito <fiorito.g@gmail.com>
2016-05-03 14:50:30 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Filmophile.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :themoviedb,
+    [app: :filmophile,
      version: "0.0.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
